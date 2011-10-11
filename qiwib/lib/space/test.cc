00ef90cc9172a03e83726d2dda6a57928fc04ba5
James Avery <avery@diku.dk>
2011-10-11 16:20:58 +1300

    Stash.

--- qiwib/lib/space/test.cc
+++ qiwib/lib/space/test.cc
@@ -34,5 +34,7 @@ int main()
   printf("int(g(x))    = % .1g + % .1g * i\n",int_g.real(), int_g.imag());
   printf("int(g'(x))   = % .1g + % .1g * i\n",int_dg.real(), int_dg.imag());
 
+  basisset<gridfunction<RealGrid1D> > basisset(Rline,10);
+  
   return 0;
 }
