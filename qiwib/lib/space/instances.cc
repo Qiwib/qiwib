3d3ff719e54e948044507308c043bceb7764f42c
James Avery <avery@diku.dk>
2011-10-11 16:11:18 +1300

    Added explicit template instantiation for basis set classes.

--- qiwib/lib/space/instances.cc
+++ qiwib/lib/space/instances.cc
@@ -4,11 +4,19 @@
 #include "space.h"
 
 #include "grid1d.cc"
+#include "basisset.hh"
+using namespace std;
 
 template class Grid1D<double>;
 template class Grid1D< std::complex<double> >;
 template class gridfunction< Grid1D<double> >;
-template class gridfunction< Grid1D< std::complex<double> > >;
+template class gridfunction< Grid1D< complex<double> > >;
+typedef gridfunction< Grid1D<double> > realfunction;
+typedef gridfunction< Grid1D< complex<double> > > complexfunction;
+
+template class basisset<realfunction>;
+template class basisset<complexfunction>;
+
 template class std::vector<double>;
 template class std::vector< std::complex<double> >;
 
