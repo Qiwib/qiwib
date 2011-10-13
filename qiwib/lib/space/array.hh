9f41ebdc24027014cd664fac0d524744786b6d14
James Avery <avery@diku.dk>
2011-10-13 16:58:42 +1300

    array.hh: Fixed typo (m->n).

--- qiwib/lib/space/array.hh
+++ qiwib/lib/space/array.hh
@@ -10,8 +10,8 @@ public:
   Array2D(size_t m, size_t n, const T *src) : std::vector<T>(src,src+m*n), m(m), n(n) {}
   Array2D(size_t m=1, size_t n=1) : std::vector<T>(m*n), m(m), n(n) {}
 
-  inline T& operator()(size_t i, size_t j){ return (*this)[i*m+j]; }
-  inline const T operator()(size_t i, size_t j) const { return (*this)[i*m+j]; }
+  inline T& operator()(size_t i, size_t j){ return (*this)[i*n+j]; }
+  inline const T operator()(size_t i, size_t j) const { return (*this)[i*n+j]; }
 
   size_t rows() const { return m; }
   size_t columns() const { return n; }
