9cbfd1ab24b5007e77e16602c3b42dc0d3607675
Thomas Ernst <t.ernst@massey.ac.nz>
2012-01-20 09:03:47 +1300

    updated copyright notice - changed year to 2012

--- qiwib/lib/kernel/Normalise.m
+++ qiwib/lib/kernel/Normalise.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2011 by Thomas Ernst
+## Copyright (C) 2012 by Thomas Ernst
 ## 
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
 ## of this software and associated documentation files (the "Software"), to deal
