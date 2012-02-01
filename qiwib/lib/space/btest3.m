fd59ded80b7bd2fa106dbc6a273ae25cae9d9ce9
James Avery <avery@diku.dk>
2012-02-01 20:06:59 +1300

    Test of complex grid.

---
+++ qiwib/lib/space/btest3.m
@@ -0,0 +1,27 @@
+#!/usr/bin/env octave
+space -global
+
+space = complexgrid(-pi,pi,2001);
+xs    = space.get_xs();
+xmin = space.xmin 
+
+phi   = complexbasis(space,4);
+phi2 = phi;
+
+
+for i = [1:4]
+  phi(i-1) = complexfunction(exp(I*i*xs)/pi);
+end
+
+plot(xs,real(phi(0).get_data()),xs,imag(phi(0).get_data()))
+pause
+
+S = phi.overlap_matrix()		# Should be pi*I
+T = -0.5*phi.laplacian_matrix()
+
+dphi = phi.derivative();
+
+plot (xs,real(dphi(0).get_data()),xs,imag(dphi(0).get_data()))
+pause;
+
+
