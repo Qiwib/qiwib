7222d2fd5a6fcbcad5e25bd4b13acaba6cd38bc4
Jacopo <dedalusj@gmail.com>
2013-03-07 08:26:37 +1000

    removed the erroneous case of temp == 31 from calc_hilbet_diff_C.cc

--- qiwib/lib/fieldsmatrices/calc_hilbert_diff_C.cc
+++ qiwib/lib/fieldsmatrices/calc_hilbert_diff_C.cc
@@ -20,6 +20,8 @@
  * THE SOFTWARE.
 */
 
+// This initialise the matrix dH_mn used to speed the computation of H_ijksql
+// See Thomas Ernst thesis (section B.3.5)
 
 #include <octave/oct.h>
 	 
@@ -50,9 +52,14 @@
            if (! error_state)
            	{ 	
 				
+				// nr is an index (from 0 to nmax) labelling the ket
 				for(int nr=0; nr<nmax; nr++)//for(int nr=0; nr<nmax; nr++)
 				{
+				
+					// Copy over the basis vector relative to nr
 					for(int i=0; i<M; i++) basisnr[i] = basis(nr,i);
+					
+					// nl is an index (from 0 to nmax) labelling the bra
 					for(int nl=0; nl<nmax; nl++)
 					{	
 
@@ -60,15 +67,21 @@
 						diffmax = 0;
 						for(int i=0; i<M; i++)
 						{
+							// Compute the difference in the occupation number of every orbital between bra and ket
 							diff[i] = basis(nl,i) - basisnr[i];							
-							if(diff[i]!=0) difflnz++;
-							if(abs(diff[i])>diffmax) diffmax = abs(diff[i]);
+							if(diff[i]!=0) difflnz++; // difflnz is a measure of how many orbitals have different occupation numbers
+							if(abs(diff[i])>diffmax) diffmax = abs(diff[i]); // diffmax is a measure of the maximum difference in occupation number
 						}
 						
+						// The only non-zero elements in H_ijksql are the ones where at most four orbitals have different 
+						// occupation number (difflnz<5) and at most two particles are shifted from one orbital (diffmax<3)
 						if(difflnz<5 && diffmax<3)
 						{
 
 							for(int i=0; i<M; i++) nindex[i] = i;					
+							
+							// Here we sort the vector of the difference of the occupation number from the biggest to the smallest
+							// we can keep track of the ordering index with the array nindex
 							for(int i=0; i<M; i++)
 								for(int j=i; j<M; j++)
 								{							
@@ -83,11 +96,22 @@
 									}
 								}
 							
+							// temp is just a convenience index useful to tag the case we are based on the
+							// values of difflnz and the highest difference in occupation number (diff[0])
+							// Much like hashing 
 							temp = 10*difflnz+diff[0];
 
 							switch(temp)
 							{
-								
+								// the structure of hilbert_diff is:
+								// hilbert_diff(0,:) is the "temp" tag we compute before to distinguish the cases
+								// hilbert_diff(1,:) is the index of the ket
+								// hilbert_diff(2,:) is the index of the bra
+								// hilbert_diff(3:M,:) is the list of orbitals with different occupation number from the largest difference to the smallest
+								// the second dimension of hilbert_diff simply number the case of non-zero elements of H_ijksql
+								// sequentially
+							
+								// this is the case where bra and ket are equal
 								case 0:
 									hilbert_diff(0,hilbert_nz) = 0;
 									hilbert_diff(1,hilbert_nz) = nr;
@@ -99,6 +123,7 @@
 									hilbert_nz++;	
 									break;
 
+								// this is the case where one particle is moved from one orbital to the other
 								case 21:
 									hilbert_diff(0,hilbert_nz) = 21;
 									hilbert_diff(1,hilbert_nz) = nr;
@@ -110,6 +135,7 @@
 									hilbert_nz++;								
 									break;
 									
+								// this is the case where two particles are moved from one orbital to the other
 								case 22:			
 									hilbert_diff(0,hilbert_nz) = 22;
 									hilbert_diff(1,hilbert_nz) = nr;
@@ -121,17 +147,20 @@
 									hilbert_nz++;
 									break;
 													
-								case 31:
-									hilbert_diff(0,hilbert_nz) = 31;
-									hilbert_diff(1,hilbert_nz) = nr;
-									hilbert_diff(2,hilbert_nz) = nl;
-									for(int i=3; i<M+3; i++)
-									{
-										hilbert_diff(i,hilbert_nz) = nindex[i-3];
-									}
-									hilbert_nz++;
-									break;
-																	
+								// This term doesn't make any sense
+								// case 31:
+// 									hilbert_diff(0,hilbert_nz) = 31;
+// 									hilbert_diff(1,hilbert_nz) = nr;
+// 									hilbert_diff(2,hilbert_nz) = nl;
+// 									for(int i=3; i<M+3; i++)
+// 									{
+// 										hilbert_diff(i,hilbert_nz) = nindex[i-3];
+// 									}
+// 									hilbert_nz++;
+// 									break;
+																
+								// this is the case where two particles are moved from one orbital
+								// into two different orbitals (parametric down conversion)
 								case 32:
 									hilbert_diff(0,hilbert_nz) = 32;
 									hilbert_diff(1,hilbert_nz) = nr;
@@ -142,7 +171,9 @@
 									}
 									hilbert_nz++;
 									break;
-																					
+									
+								// This is the case where we transfer one particle each from two
+								// different orbitals and put the in another two different orbitals													
 								case 41:
 									hilbert_diff(0,hilbert_nz) = 41;
 									hilbert_diff(1,hilbert_nz) = nr;
@@ -156,6 +187,7 @@
 																
 							}
 							
+							// if we hit the maximum size of the hilbert_diff matrix just allocate some more
 							if (hilbert_nz==hilbert_nz_max)
 								{
 									hilbert_diff.resize(M+3,2*hilbert_nz);
